bffa4a2478a2314792e17805721dbc5b5458dc81
freeoneed <freeoneed@gmail.com>
2022-03-14 17:02:58 +0300

    add deb repo for astralinux and add bashrc_custom

--- new.sh
+++ new.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+#add repo
+if grep -e debian /etc/apt/sources.list; then.
+    echo "[INFO] Repositpry already exist!"
+else
+    echo "deb http://ftp.debian.org/debian stretch main contrib non-free" >> /etc/apt/sources.list
+    echo "deb-src http://ftp.debian.org/debian stretch main contrib non-free" >> /etc/apt/sources.list
+    echo "[INFO] Repository update!"
+fi
+
 echo "[INFO] Start script..."
 sudo apt update
 echo "[INFO] Update packages complete! Try install..."
@@ -10,5 +20,13 @@ sudo cp custom_conf/.tmux.conf ~/
 source ~/.bashrc_custom
 echo '[INFO] OK! Customize tmux...'
 tmux source-file ~/.tmux.conf
+
+#add bashrc_custom to main bashrc
+if grep -e bashrc_custom ~/.bashrc; then
+    echo "[INFO] Custom file already exist!"
+else
+    echo "source ~/.bashrc_custom" >> ~/.bashrc
+fi
+
 echo '[INFO] All done!'
 echo '[INFO] Enter "source ~/.bashrc" for complete install or relogin!'
